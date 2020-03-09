00e849d459208c54cdb212f55db3d9f88d2da30b
kage1029 <ichika789rider@gmail.com>
2020-03-10 07:50:24 +0900

    Move test_helper.rb for heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,15 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
 
-<<<<<<< HEAD
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-=======
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password reset"
->>>>>>> password-reset
   end
 end
